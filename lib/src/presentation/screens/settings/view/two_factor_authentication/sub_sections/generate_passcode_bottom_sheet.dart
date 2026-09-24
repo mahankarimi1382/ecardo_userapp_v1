5780a035c9f46f1d13406fca6e6171d202960e74
mahankarimi1382 <160334368+mahankarimi1382@users.noreply.github.com>
2026-09-24 15:43:33 +0330

    fix(security): generate passcode sheet maxDigits from PasscodeHelper (4–6)
    Agent: github-user

--- lib/src/presentation/screens/settings/view/two_factor_authentication/sub_sections/generate_passcode_bottom_sheet.dart
+++ lib/src/presentation/screens/settings/view/two_factor_authentication/sub_sections/generate_passcode_bottom_sheet.dart
@@ -7,6 +7,7 @@ import 'package:ecardo_user/src/app/constants/assets_path/png/png_assets.dart';
 import 'package:ecardo_user/src/common/widgets/button/common_button.dart';
 import 'package:ecardo_user/src/common/widgets/common_required_label_and_dynamic_field.dart';
 import 'package:ecardo_user/src/common/widgets/input_field/common_text_input_filed.dart';
+import 'package:ecardo_user/src/helper/passcode_helper.dart';
 import 'package:ecardo_user/src/presentation/screens/settings/controller/two_factor_authentication_controller.dart';
 
 class GeneratePasscodeBottomSheet extends StatefulWidget {
@@ -21,10 +22,10 @@ class _GeneratePasscodeBottomSheetState
     extends State<GeneratePasscodeBottomSheet> {
   final TwoFactorAuthenticationController controller = Get.find();
 
-  static final _digitLimit = [
-    FilteringTextInputFormatter.digitsOnly,
-    LengthLimitingTextInputFormatter(4),
-  ];
+  List<TextInputFormatter> get _digitLimit => [
+        FilteringTextInputFormatter.digitsOnly,
+        LengthLimitingTextInputFormatter(PasscodeHelper.maxDigits),
+      ];
 
   @override
   Widget build(BuildContext context) {
